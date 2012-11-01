4bcd7a362f89f486f7e627442d42f4080fdda315
Wim Rutgeerts <wimrutgeerts@Wims-MacBook-Pro.local>
2012-11-01 23:37:34 +0100

    initial commit

---
+++ flatfrog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flatfrog::Application.config.secret_token = 'a08f2c30801ecc0ddf014e4ac73e6b82caa96b906cfa1c4ec5ffdd613f07059743fe8567945f7e051aff8d09c8ca1fa23f92f012295599b4c78a0f24608474cb'
